b008018a67fc583f1ef3533ac870cbb37d771ab7
Nelson Lai <me@nelsonlai.dev>
2025-09-22 23:24:39 +0800

    test(web): make the render function reusable

--- apps/web/src/tests/components/home/hero.test.tsx
+++ apps/web/src/tests/components/home/hero.test.tsx
@@ -1,18 +1,13 @@
-import { NextIntlClientProvider } from '@repo/i18n/client'
-import messages from '@repo/i18n/messages/en.json'
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import { describe, expect, it } from 'vitest'
 
 import Hero from '@/components/home/hero'
 import { MY_NAME } from '@/lib/constants'
+import { render } from '@/utils/render'
 
 describe('<Hero />', () => {
   it('should have a hero image', () => {
-    render(
-      <NextIntlClientProvider locale='en' messages={messages}>
-        <Hero />
-      </NextIntlClientProvider>
-    )
+    render(<Hero />)
 
     expect(screen.getByAltText(`${MY_NAME}'s Logo`)).toBeInTheDocument()
   })
