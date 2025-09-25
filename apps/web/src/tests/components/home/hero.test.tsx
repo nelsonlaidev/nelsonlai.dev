0a1e8f89dac0a886220b8e9e49b11268be42e372
Nelson Lai <me@nelsonlai.dev>
2025-09-25 12:55:19 +0800

    refactor: replace `it` with `test` in unit tests

--- apps/web/src/tests/components/home/hero.test.tsx
+++ apps/web/src/tests/components/home/hero.test.tsx
@@ -1,12 +1,12 @@
 import { screen } from '@testing-library/react'
-import { describe, expect, it } from 'vitest'
+import { describe, expect, test } from 'vitest'
 
 import Hero from '@/components/home/hero'
 import { MY_NAME } from '@/lib/constants'
 import { render } from '@/utils/render'
 
 describe('<Hero />', () => {
-  it('should have a hero image', () => {
+  test('has a hero image', () => {
     render(<Hero />)
 
     expect(screen.getByAltText(`${MY_NAME}'s Logo`)).toBeInTheDocument()
