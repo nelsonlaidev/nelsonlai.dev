3ba6b6fe53597e485de5997ee46e501a450a6df2
Nelson Lai <me@nelsonlai.dev>
2025-10-01 18:07:15 +0800

    refactor: use next-intl correctly

--- apps/web/src/app/[locale]/(admin)/admin/comments/page.tsx
+++ apps/web/src/app/[locale]/(admin)/admin/comments/page.tsx
@@ -1,14 +1,25 @@
-'use client'
+import { routing } from '@repo/i18n/routing'
+import { notFound } from 'next/navigation'
+import { hasLocale } from 'next-intl'
+import { getTranslations, setRequestLocale } from 'next-intl/server'
 
-import { useTranslations } from 'next-intl'
-
+import AdminComments from '@/components/admin/admin-comments'
 import AdminPageHeader from '@/components/admin/admin-page-header'
-import CommentsTable from '@/components/tables/comments'
-import { useAdminComments } from '@/hooks/queries/admin.query'
 
-const Page = () => {
-  const { isSuccess, isLoading, isError, data } = useAdminComments()
-  const t = useTranslations()
+export const generateStaticParams = (): Array<{ locale: string }> => {
+  return routing.locales.map((locale) => ({ locale }))
+}
+
+const Page = async (props: PageProps<'/[locale]/admin/comments'>) => {
+  const { params } = props
+  const { locale } = await params
+
+  if (!hasLocale(routing.locales, locale)) {
+    notFound()
+  }
+
+  setRequestLocale(locale)
+  const t = await getTranslations()
 
   return (
     <div className='space-y-6'>
@@ -16,9 +27,7 @@ const Page = () => {
         title={t('admin.page-header.comments.title')}
         description={t('admin.page-header.comments.description')}
       />
-      {isSuccess && <CommentsTable comments={data.comments} />}
-      {isLoading && 'Loading...'}
-      {isError && <div>{t('error.failed-to-fetch-comments-data')}</div>}
+      <AdminComments />
     </div>
   )
 }
--- apps/web/src/app/[locale]/(admin)/admin/page.tsx
+++ apps/web/src/app/[locale]/(admin)/admin/page.tsx
@@ -1,4 +1,22 @@
-const Page = () => {
+import { routing } from '@repo/i18n/routing'
+import { notFound } from 'next/navigation'
+import { hasLocale } from 'next-intl'
+import { setRequestLocale } from 'next-intl/server'
+
+export const generateStaticParams = (): Array<{ locale: string }> => {
+  return routing.locales.map((locale) => ({ locale }))
+}
+
+const Page = async (props: PageProps<'/[locale]/admin'>) => {
+  const { params } = props
+  const { locale } = await params
+
+  if (!hasLocale(routing.locales, locale)) {
+    notFound()
+  }
+
+  setRequestLocale(locale)
+
   return <div>admin</div>
 }
 
