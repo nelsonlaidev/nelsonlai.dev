b80761e4e93a77a31b5cf55c9e73c0bc81797c14
Nelson Lai <me@nelsonlai.dev>
2025-10-12 16:39:09 +0800

    feat: mobile nav on account settings page

--- apps/web/src/app/[locale]/(account)/layout.tsx
+++ apps/web/src/app/[locale]/(account)/layout.tsx
@@ -3,6 +3,7 @@ import type { Locale } from 'next-intl'
 import { redirect } from '@repo/i18n/routing'
 import { getTranslations } from 'next-intl/server'
 
+import AccountMobileNav from '@/components/account/account-mobile-nav'
 import AccountSidebar from '@/components/account/account-sidebar'
 import MainLayout from '@/components/main-layout'
 import PageHeader from '@/components/page-header'
@@ -24,7 +25,10 @@ const Layout = async (props: LayoutProps<'/[locale]'>) => {
 
   return (
     <MainLayout>
-      <PageHeader title={title} description={description} />
+      <div className='flex items-center justify-between'>
+        <PageHeader title={title} description={description} />
+        <AccountMobileNav />
+      </div>
       <div className='gap-10 md:flex'>
         <AccountSidebar />
         <div className='w-full space-y-12'>{children}</div>
