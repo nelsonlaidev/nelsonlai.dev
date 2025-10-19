b8e83a86ff2502cc80dae7a44dd0c809b90f353d
Nelson Lai <me@nelsonlai.dev>
2025-10-19 16:45:59 +0800

    feat(docs): add components and preview

--- apps/docs/src/mdx-components.ts
+++ apps/docs/src/mdx-components.ts
@@ -2,9 +2,12 @@ import type { MDXComponents } from 'mdx/types'
 
 import defaultMdxComponents from 'fumadocs-ui/mdx'
 
+import ComponentPreview from '@/components/component-preview'
+
 export const getMDXComponents = (components?: MDXComponents): MDXComponents => {
   return {
     ...defaultMdxComponents,
-    ...components
+    ...components,
+    ComponentPreview
   }
 }
