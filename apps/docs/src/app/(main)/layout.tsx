16a233bb8fb51f328de3be7cb8847a5a8bfdacfc
Nelson Lai <me@nelsonlai.dev>
2025-10-20 19:00:16 +0800

    refactor(docs): move ui to folder

--- apps/docs/src/app/(main)/layout.tsx
+++ apps/docs/src/app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import { Logo } from '@repo/ui/components/logo'
 import { DocsLayout } from 'fumadocs-ui/layouts/notebook'
 
 import { source } from '@/lib/source'
@@ -6,7 +7,17 @@ const Layout = (props: LayoutProps<'/'>) => {
   const { children } = props
 
   return (
-    <DocsLayout tree={source.pageTree} nav={{ title: '@nelsonlaidev/docs' }}>
+    <DocsLayout
+      tree={source.pageTree}
+      nav={{
+        title: (
+          <div className='flex items-center gap-2.5 pl-2.5 font-mono'>
+            <Logo width={12} />
+            @nelsonlaidev/docs
+          </div>
+        )
+      }}
+    >
       {children}
     </DocsLayout>
   )
