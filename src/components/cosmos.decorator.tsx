04dd89b5bebf7680b668b58c98283ca88f629edd
Nelson Lai <me@nelsonlai.dev>
2026-03-05 11:06:48 +0800

    fix(cosmos): wrap Decorator component with NextIntlClientProvider

--- src/components/cosmos.decorator.tsx
+++ src/components/cosmos.decorator.tsx
@@ -1,15 +1,19 @@
 import type { DecoratorProps } from 'react-cosmos-core'
 
+import { NextIntlClientProvider } from 'next-intl'
+
 import { Toaster } from './ui/sonner'
 
 function Decorator(props: DecoratorProps) {
   const { children } = props
 
   return (
-    <div className='mx-auto w-full max-w-3xl p-8'>
-      {children}
-      <Toaster />
-    </div>
+    <NextIntlClientProvider>
+      <div className='mx-auto w-full max-w-3xl p-8'>
+        {children}
+        <Toaster />
+      </div>
+    </NextIntlClientProvider>
   )
 }
 
