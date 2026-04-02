afc5eafade32dff4cadbe5fffff19a0fcaadc216
Nelson Lai <me@nelsonlai.dev>
2026-04-03 00:50:11 +0800

    refactor: update postcss config

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,9 +1,5 @@
-import type { Config } from 'postcss-load-config'
-
-const config: Config = {
-  plugins: {
-    '@tailwindcss/postcss': {},
-  },
+const config = {
+  plugins: ['@tailwindcss/postcss'],
 }
 
 export default config
