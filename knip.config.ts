912243283ec57c066d07f85310b5346921b54e2b
Nelson Lai <me@nelsonlai.dev>
2026-07-05 09:18:07 +0800

    chore: remove tsx

--- knip.config.ts
+++ knip.config.ts
@@ -1,10 +1,6 @@
 import type { KnipConfig } from 'knip'
 
 const config: KnipConfig = {
-  ignoreDependencies: [
-    // Knip can't detect it since we added "bun with-env" before the scripts
-    'tsx',
-  ],
   ignore: ['src/components/ui/*.{ts,tsx}'],
   entry: ['content-collections.ts', 'src/db/reset.ts', 'src/db/seed.ts'],
 }
